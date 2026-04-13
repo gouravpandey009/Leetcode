a13eaaa7612dbc182f2f42c165a418dc5143fd82
Gourav Pandey <21cs3019@rgipt.ac.in>
2026-04-13 06:25:52 +0530

    min_distance

---
+++ greedy/1848_minimum_distance_to_target.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int getMinDistance(vector<int>& nums, int target, int start) {
+    int ans = INT_MAX;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == target) {
+            ans = min(ans, abs(i - start));
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4,5};
+    int target = 5, start = 3;
+
+    cout << getMinDistance(nums, target, start) << endl; // Output: 1
+}
