02591773deadc3f98318dd1f368d113047dcb8b3
Gourav Pandey <21cs3019@rgipt.ac.in>
2026-05-02 09:06:45 +0530

    rotated_digits

---
+++ Math/digit_manipulation/788_rotated_digits.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int rotatedDigits(int n) {
+    int count = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int num = i;
+        bool valid = true;
+        bool changed = false;
+
+        while (num > 0) {
+            int digit = num % 10;
+
+            if (digit == 3 || digit == 4 || digit == 7) {
+                valid = false;
+                break;
+            }
+
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                changed = true;
+            }
+
+            num /= 10;
+        }
+
+        if (valid && changed) count++;
+    }
+
+    return count;
+}
+
+int main() {
+    int n = 10;
+    cout << rotatedDigits(n) << endl;
+    return 0;
+}
