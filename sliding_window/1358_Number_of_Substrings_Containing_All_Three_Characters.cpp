cd57980bbb5c68e8739d454e4188795bc1b9a0ec
gouravpandey009 <21cs3019@rgipt.ac.in>
2026-06-30 07:19:03 +0530

    no_of_substrings_all_3_char

---
+++ sliding_window/1358_Number_of_Substrings_Containing_All_Three_Characters.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+
+        vector<int> freq(3, 0);
+
+        int left = 0;
+        int ans = 0;
+        int n = s.size();
+
+        for (int right = 0; right < n; right++) {
+
+            freq[s[right] - 'a']++;
+
+            while (freq[0] > 0 &&
+                   freq[1] > 0 &&
+                   freq[2] > 0) {
+
+                ans += (n - right);
+
+                freq[s[left] - 'a']--;
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s = "abcabc";
+
+    cout << "Answer = "
+         << obj.numberOfSubstrings(s)
+         << endl;
+
+    return 0;
+}
