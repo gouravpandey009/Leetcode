f143daff05db7606e35d01729a206e840c222dd8
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-07-08 07:02:20 +0530

    Time: 47 ms (46.63%) | Memory: 149.1 MB (80.31%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -1,52 +1,74 @@
 class Solution {
 public:
+    static constexpr int MOD = 1000000007;
+
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-        const int MOD = 1e9 + 7;
         int n = s.size();
 
+        // Prefix sum of digits
         vector<int> prefSum(n + 1, 0);
-
         for (int i = 0; i < n; i++)
             prefSum[i + 1] = prefSum[i] + (s[i] - '0');
 
-        vector<int> pos, digit;
+        // Non-zero digits and their positions
+        vector<int> pos;
+        vector<int> prefHash;
+        pos.reserve(n);
+        prefHash.reserve(n + 1);
+
+        prefHash.push_back(0);
+
         for (int i = 0; i < n; i++) {
             if (s[i] != '0') {
                 pos.push_back(i);
-                digit.push_back(s[i] - '0');
+                prefHash.push_back((10LL * prefHash.back() + (s[i] - '0')) % MOD);
             }
         }
 
-        int m = digit.size();
+        int m = pos.size();
 
-        vector<long long> pw(m + 1, 1);
+        // powers of 10
+        vector<int> pw(m + 1, 1);
         for (int i = 1; i <= m; i++)
-            pw[i] = pw[i - 1] * 10 % MOD;
+            pw[i] = 10LL * pw[i - 1] % MOD;
 
-        vector<long long> pref(m + 1, 0);
-        for (int i = 0; i < m; i++)
-            pref[i + 1] = (pref[i] * 10 + digit[i]) % MOD;
+        // leftId[i] = first non-zero digit id >= i
+        vector<int> leftId(n, -1);
+        int p = m - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            while (p >= 0 && pos[p] >= i) p--;
+            leftId[i] = (p + 1 < m) ? p + 1 : -1;
+        }
+
+        // rightId[i] = last non-zero digit id <= i
+        vector<int> rightId(n, -1);
+        p = 0;
+        for (int i = 0; i < n; i++) {
+            while (p < m && pos[p] <= i) p++;
+            rightId[i] = p - 1;
+        }
 
         vector<int> ans;
+        ans.reserve(queries.size());
 
         for (auto &q : queries) {
-            int l = q[0], r = q[1];
+            int l = q[0];
+            int r = q[1];
 
-            long long sum = prefSum[r + 1] - prefSum[l];
+            int sum = prefSum[r + 1] - prefSum[l];
 
-            auto it1 = lower_bound(pos.begin(), pos.end(), l);
-            auto it2 = upper_bound(pos.begin(), pos.end(), r);
+            int L = leftId[l];
+            int R = rightId[r];
 
-            if (it1 == it2) {
+            if (L == -1 || R == -1 || L > R) {
                 ans.push_back(0);
                 continue;
             }
 
-            int L = it1 - pos.begin();
-            int R = int(it2 - pos.begin()) - 1;
-            int len = R - L + 1;
+            long long val =
+                (prefHash[R + 1] -
+                 1LL * prefHash[L] * pw[R - L + 1]) % MOD;
 
-            long long val = (pref[R + 1] - pref[L] * pw[len]) % MOD;
             if (val < 0) val += MOD;
 
             ans.push_back(val * sum % MOD);
