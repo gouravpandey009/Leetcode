82649dfbd16a697bbbcd005f45688f1fdc6e64f9
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-07-11 07:45:34 +0530

    Time: 27 ms (88.30%) | Memory: 137.2 MB (32.02%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
 
-        vector<vector<int>> g(n);
+        vector<vector<int>> graph(n);
 
+        // Build the graph.
         for (auto &e : edges) {
-            g[e[0]].push_back(e[1]);
-            g[e[1]].push_back(e[0]);
+            graph[e[0]].push_back(e[1]);
+            graph[e[1]].push_back(e[0]);
         }
 
-        vector<int> vis(n);
-        int ans = 0;
+        vector<int> vis(n, 0);
+        int complete = 0;
 
         for (int i = 0; i < n; i++) {
 
@@ -29,9 +30,11 @@ public:
                 q.pop();
 
                 nodes++;
-                degreeSum += g[u].size();
 
-                for (int v : g[u]) {
+                // Every adjacent node contributes one degree.
+                degreeSum += graph[u].size();
+
+                for (int v : graph[u]) {
                     if (!vis[v]) {
                         vis[v] = 1;
                         q.push(v);
@@ -39,10 +42,13 @@ public:
                 }
             }
 
+            // Complete graph with k nodes has:
+            // k * (k - 1) / 2 edges.
+            // Degree sum counts every edge twice.
             if (degreeSum == nodes * (nodes - 1))
-                ans++;
+                complete++;
         }
 
-        return ans;
+        return complete;
     }
 };
