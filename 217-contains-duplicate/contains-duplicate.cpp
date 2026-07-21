2862d4f223c2c2d32d0c1c284923b77e241e48aa
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-07-21 08:30:30 +0530

    Time: 16 ms (97.97%) | Memory: 82.7 MB (82.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        unordered_set<int> seen;
+        sort(nums.begin(), nums.end());
 
-        for (int number : nums) {
+        for (int i = 1; i < nums.size(); i++) {
 
-            // Duplicate found
-            if (seen.count(number))
+            if (nums[i] == nums[i - 1])
                 return true;
-
-            // Store current number
-            seen.insert(number);
         }
 
         return false;
