43f949f9fb9893bdf37bea046fa8b8df2befdf91
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-07-23 10:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 198.7 MB (98.13%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        size_t n = nums.size();
 
-    return 1 << (bit_width(n) - 3 / (n + 1));
+        int n = nums.size();
+
+        // Cannot choose three distinct elements.
+        if (n < 3)
+            return n;
+
+        int bits = 32 - __builtin_clz(n);
+
+        // All values in [0, 2^bits - 1] are achievable.
+        return 1 << bits;
     }
 };
