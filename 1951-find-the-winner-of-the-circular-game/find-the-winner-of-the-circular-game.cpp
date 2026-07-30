122509a3a3feb64efa6b01bf9d8d3f74e9dc903f
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-07-30 22:24:47 +0530

    Time: 21 ms (21.57%) | Memory: 26.5 MB (5.14%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-        int helper(int n , int k){
-            if(n == 1)
-            return 0;
+    int findTheWinner(int n, int k) {
 
-            return (helper(n - 1 , k) + k) % n;
+        // Queue stores all players
+        queue<int> players;
+
+        // Initially every player is alive
+        for (int player = 1; player <= n; player++) {
+            players.push(player);
         }
 
-        int findTheWinner(int n, int k) {
-            return helper(n , k) + 1;
+        // Continue until only one player survives
+        while (players.size() > 1) {
 
+            // Rotate the first (k-1) players
+            // by moving them from front to back.
+            for (int count = 1; count < k; count++) {
+
+                players.push(players.front());
+                players.pop();
+            }
+
+            // The kth player is eliminated.
+            players.pop();
+        }
+
+        // Last remaining player is the winner.
+        return players.front();
     }
 };
