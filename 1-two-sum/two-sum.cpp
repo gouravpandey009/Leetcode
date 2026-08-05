801a5ade7536d706145d378fd74866d188e70af4
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-05 12:29:55 +0530

    Time: 35 ms (35.10%) | Memory: 14.2 MB (64.38%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,38 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-    
-        unordered_map<int, int> valueToIndex;
-
-        // Process each element exactly once.
-        for (int currentIndex = 0; currentIndex < (nums.size()); ++currentIndex) {
-
-            // Current value under consideration.
-            int currentValue = nums[currentIndex];
-
-            // If currentValue + complement = target,
-            // then complement = target - currentValue.
-            int complement = target - currentValue;
-
-            // Before storing the current value, check whether the required
-            // complement has already appeared.
-            auto iterator = valueToIndex.find(complement);
-
-            if (iterator != valueToIndex.end()) {
-
-                // The complement already exists.
-                // Return its index along with the current index.
-                return {iterator->second, currentIndex};
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
             }
-
-            // Store the current value after checking.
-            // This prevents using the same element twice.
-            valueToIndex[currentValue] = currentIndex;
-        }
-
-        // Problem guarantees an answer.
-        // This return is only to satisfy the compiler.
-        return {};
+        }return {};
     }
 };
