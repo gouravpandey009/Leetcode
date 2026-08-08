2ac7d490f70440d1ede13fbf0fea6658dd09118f
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-08 12:58:31 +0530

    Time: 36 ms (85.11%) | Memory: 101.1 MB (85.11%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
-        int N = word1.size();
-        int M = word2.size();
 
-        int R = M - 1;
-        int C = 0;
+        int n = word1.size();
+        int m = word2.size();
 
-        vector<int> Right(N);
-        for(int i = N - 1 ; i >= 0 ; i--){
-            Right[i] = C;
-            if(R >= 0 && word1[i] == word2[R]){
-                R--;
-                C++;
+        vector<int> last(m, -1);
+
+        // Latest positions that can match each suffix character.
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (word1[i] == word2[j])
+                last[j--] = i;
+
+            i--;
+        }
+
+        vector<int> ans;
+        ans.reserve(m);
+
+        bool usedMismatch = false;
+        j = 0;
+
+        for (i = 0; i < n && j < m; i++) {
+
+            if (word1[i] == word2[j]) {
+
+                ans.push_back(i);
+                j++;
+
+            } else if (!usedMismatch &&
+                       (j == m - 1 || i < last[j + 1])) {
+
+                // Use the only allowed mismatch here.
+                ans.push_back(i);
+                j++;
+                usedMismatch = true;
             }
         }
 
-        vector<int>ans;
-        bool changed = false;
-        int j = 0;
-
-        for(int i = 0 ; i < N && j < M ; i++){
-            if(word1[i] == word2[j]){
-                ans.push_back(i);
-                j++;
-            } else if(!changed && Right[i] >= M - 1 - j){
-                ans.push_back(i);
-                j++;
-                changed = true;
-            }
-        }
-
-        if(j == M){
-            return ans;
-        }
-
-        return {};
+        return j == m ? ans : vector<int>();
     }
 };
