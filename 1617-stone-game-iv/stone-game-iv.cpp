b9f084b0075fe4e8a9adc652c4b1331ee6ebbcaa
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-10 14:17:09 +0530

    Time: 23 ms (89.10%) | Memory: 9.8 MB (70.69%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool winnerSquareGame(int n) {
-        vector<bool> dp(n + 1 , false);
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j * j <= i ; j++){
-                if(!dp[i - j * j]){
+        vector<char> dp(n + 1, false);
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j * j <= i; ++j) {
+                if (!dp[i - j * j]) {
                     dp[i] = true;
                     break;
                 }
