eb3f826b6c422986928e0cbf8cea337dabc5f75b
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-21 07:27:51 +0530

    Time: 11 ms (81.40%) | Memory: 23 MB (5.43%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,96 +1,66 @@
 class Solution {
-public:
-    long long gcd(long long a  , long long b){
-        while(b != 0){
-            long long temp = a % b;
+    long long gcd(long long a, long long b) {
+        while (b) {
+            long long t = a % b;
             a = b;
-            b = temp;
+            b = t;
         }
         return a;
     }
-    
-    long long lcm(long long a , long long b){
-        return a  / gcd(a , b) * b;
+
+    long long lcm(long long a, long long b) {
+        return a / gcd(a, b) * b;
     }
 
-    long long solve(long long mid , vector<int>& coins)
-    {
-        sort(coins.begin() , coins.end());
-
-        vector<int>f;
-        
-        for(int i = 0 ; i < coins.size() ; i++){
-            bool red = false;
-
-            for(int z : f){
-                if(coins[i] % z == 0){
-                    red = true;
-                    break;
-                }
-            }
-
-            if(!red)
-            f.push_back(coins[i]);
-        }
-
-        coins = f;
-
-        int n = coins.size();
-        long long ans = 0;
-        
-
-        for(int mask = 1; mask <(1 << n) ; mask++){
-            int bits = 0;
-            long long x = 1;
-            bool check = false;
-
-
-            for(int j = 0 ; j < n ; j++){
-                if(mask & (1 << j)){
-                    x = lcm(x , coins[j]);
-                    bits++;
-
-                    if(x > mid){
-                        check = true;
-                        break;
-                    }
-                }
-            }
-
-            if(check)
-                continue;
-
-                long long curr = mid / x;
-
-                if(bits % 2 )
-                ans += curr;
-
-                else 
-                ans -= curr;
-        }
-
-        return ans;
-    }
-
-
-
-
+public:
     long long findKthSmallest(vector<int>& coins, int k) {
         int n = coins.size();
-        int mini = *min_element(coins.begin() , coins.end());
 
-        long long low = 1;
-        long long high = 1LL * mini * k;
+        vector<pair<long long, int>> subsets;
 
-        while(low <= high){
-            long long mid = low + (high - low) / 2;
+        for (int mask = 1; mask < (1 << n); ++mask) {
+            long long curLcm = 1;
+            int bits = 0;
 
-            if(solve(mid , coins) >= k)
-            high = mid - 1;
-            else
-                low = mid + 1;
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {
+                    ++bits;
+                    curLcm = lcm(curLcm, coins[i]);
+                }
+            }
+
+            subsets.push_back({
+                curLcm,
+                bits & 1 ? 1 : -1
+            });
         }
 
-        return low;
+        auto count = [&](long long x) {
+            long long total = 0;
+
+            for (auto [v, sign] : subsets) {
+                if (v > x)
+                    continue;
+
+                total += sign * (x / v);
+            }
+
+            return total;
+        };
+
+        long long left = 1;
+        long long right = 1LL * k * (*min_element(coins.begin(),
+                                                   coins.end()));
+
+        while (left < right) {
+            long long mid = left + (right - left) / 2;
+
+            if (count(mid) >= k)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
     }
 };
