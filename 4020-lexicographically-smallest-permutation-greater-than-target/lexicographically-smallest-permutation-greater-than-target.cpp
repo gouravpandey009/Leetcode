9c3816b297bbfcd2ded942aa5e8edf016cb182e9
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-27 19:46:25 +0530

    Time: 3 ms (69.43%) | Memory: 9.9 MB (82.17%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,68 +1,44 @@
 class Solution {
 public:
     string lexGreaterPermutation(string s, string target) {
+        int cnt[26] = {};
+
+        for (char c : s)
+            ++cnt[c - 'a'];
+
+        string quinorath = s;
+
         int n = s.size();
-        vector<int> cnt(26 , 0);
+        int i = 0;
 
-        for(char c  : s){
-            cnt[c - 'a']++;
+        while (i < n && cnt[target[i] - 'a'] > 0) {
+            --cnt[target[i] - 'a'];
+            ++i;
         }
 
-        string prefix;
+        while (true) {
+            if (i < n) {
+                for (int c = target[i] - 'a' + 1; c < 26; ++c) {
+                    if (cnt[c] == 0)
+                        continue;
 
-        for(int i = 0 ; i < n ; i++){
-            int x = target[i] - 'a';
+                    string ans = target.substr(0, i);
+                    ans.push_back('a' + c);
 
-            if(cnt[x] == 0){
+                    --cnt[c];
+
+                    for (int j = 0; j < 26; ++j)
+                        ans.append(cnt[j], 'a' + j);
+
+                    return ans;
+                }
+            }
+
+            if (i == 0)
                 break;
-            }
 
-            cnt[x]--;
-            prefix += target[i];
-        }
-
-        if((int)prefix.size() < n){
-            int i = prefix.size();
-            int x = target[i] - 'a';
-
-            for(int c = x + 1 ; c < 26 ; c++){
-                if(cnt[c] == 0)
-                continue;
-
-                string ans = prefix;
-                ans += char('a' + c);
-
-                cnt[c]--;
-
-                for(int ch = 0 ; ch < 26 ; ch++){
-                    ans.append(cnt[ch] , char('a' + ch));
-                }
-
-                return ans;
-            }
-        }
-
-        for(int i = prefix.size() - 1 ;  i >= 0 ; i--){
-            cnt[prefix[i] - 'a']++;
-            prefix.pop_back();
-
-            int x = target[i] - 'a';
-
-            for(int c = x + 1 ; c < 26 ; c++){
-                if(cnt[c] == 0)
-                continue;
-
-                string ans = prefix;
-
-                ans += char('a' + c);
-                cnt[c]--;
-
-                for(int ch = 0 ; ch < 26 ; ch++){
-                    ans.append(cnt[ch] , char('a' + ch));
-                }
-
-                return ans;
-            }
+            --i;
+            ++cnt[target[i] - 'a'];
         }
 
         return "";
