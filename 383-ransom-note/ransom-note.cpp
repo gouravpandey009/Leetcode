fc4969f0b6b8040a8afdfeb7fa3b9eb15d0efed3
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-31 12:24:16 +0530

    Time: 1 ms (62.97%) | Memory: 11.8 MB (74.31%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        int freq[26] = {};
+
+        for(char ch : magazine){
+            freq[ch - 'a']++;
+        }
+
+
+        for(char ch : ransomNote){
+            if(freq[ch - 'a'] == 0){
+                return false;
+            }
+
+            freq[ch- 'a']--;
+        }
+
+        return true;
+        
+    }
+};
