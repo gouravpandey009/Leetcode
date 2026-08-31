68a917a1f3a917471607f868c3071aeca899b810
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-08-31 08:10:16 +0530

    Time: 4 ms (57.39%) | Memory: 124.6 MB (81.63%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> nums;
-
-        while (head) {
-            nums.push_back(head->val);
-            head = head->next;
-        }
-
-        vector<int> criticalPoints;
-        int n = nums.size();
-
-        // Stop at n - 1 so nums[i + 1] stays valid
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-                criticalPoints.push_back(i);
-            }
-            else if (nums[i] < nums[i - 1] && nums[i] < nums[i + 1]) {
-                criticalPoints.push_back(i);
-            }
-        }
-
-        int m = criticalPoints.size();
-        if (m < 2) return {-1, -1};
-
+        int first = -1;
+        int last = -1;
         int minDist = INT_MAX;
-        int maxDist = criticalPoints[m - 1] - criticalPoints[0];
 
-        for (int i = 1; i < m; i++) {
-            minDist = min(minDist, criticalPoints[i] - criticalPoints[i - 1]);
+        int pos = 1;
+
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+
+        while (curr && curr->next) {
+            ListNode* next = curr->next;
+
+            if (
+                (curr->val > prev->val && curr->val > next->val) ||
+                (curr->val < prev->val && curr->val < next->val)
+            ) {
+                if (first == -1)
+                    first = pos;
+
+                if (last != -1)
+                    minDist = min(minDist, pos - last);
+
+                last = pos;
+            }
+
+            prev = curr;
+            curr = next;
+            ++pos;
         }
 
-        return {minDist, maxDist};
+        if (first == -1 || first == last)
+            return {-1, -1};
+
+        return {minDist, last - first};
     }
 };
