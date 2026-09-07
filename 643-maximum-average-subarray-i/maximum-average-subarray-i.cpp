c3d200da69d42c9b30432e268191df9c4c1f1017
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-09-07 12:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 113.6 MB (86.88%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -3,21 +3,22 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         int sum = 0;
 
-        // Find sum of the first k elements.
-        for (int i = 0; i < k; i++) {
+        for(int i = 0 ; i < k ; i++){
             sum += nums[i];
         }
 
         int maxSum = sum;
 
-        // Slide the window through the array.
-        for (int i = k; i < nums.size(); i++) {
-            sum -= nums[i - k];  // Remove old element.
-            sum += nums[i];      // Add new element.
+        for(int i = k ; i < nums.size() ; i++){
+            sum -= nums[i - k];
+            sum += nums[i];
 
-            maxSum = max(maxSum, sum);
+            maxSum = max(maxSum , sum);
         }
 
         return (double)maxSum / k;
+
+
+        
     }
 };
