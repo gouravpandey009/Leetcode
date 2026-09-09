fb9e8401a0b2f89c92cfcaa9b20a1c3044825218
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-09-09 11:24:19 +0530

    Time: 8 ms (36.30%) | Memory: 13.1 MB (58.68%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
+        
         int count = 0;
 
-        // Count vowels in the first window.
-        for (int i = 0; i < k; i++) {
-            if (s[i] == 'a' || s[i] == 'e' ||
-                s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u') {
+        for(int i = 0 ; i < k ; i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i]  == 'i' || s[i] == 'o' || s[i] == 'u'){
                 count++;
             }
         }
 
-        int maxVowels = count;
+         int maxVowels = count;
 
-        // Slide the window.
-        for (int i = k; i < s.size(); i++) {
-
-            // Remove the character leaving the window.
-            if (s[i - k] == 'a' || s[i - k] == 'e' ||
-                s[i - k] == 'i' || s[i - k] == 'o' ||
-                s[i - k] == 'u') {
+        for(int i = k ; i < s.size() ; i++){
+            if(s[i - k] == 'a' || s[i - k] == 'e' || s[i - k]  == 'i' || s[i - k] == 'o' || s[i - k] == 'u'){
                 count--;
             }
 
-            // Add the new character entering the window.
-            if (s[i] == 'a' || s[i] == 'e' ||
-                s[i] == 'i' || s[i] == 'o' ||
-                s[i] == 'u') {
+            if(s[i] == 'a' || s[i] == 'e' || s[i]  == 'i' || s[i] == 'o' || s[i] == 'u'){
                 count++;
             }
 
-            maxVowels = max(maxVowels, count);
+            maxVowels = max(count , maxVowels);
         }
 
         return maxVowels;
+        
     }
 };
