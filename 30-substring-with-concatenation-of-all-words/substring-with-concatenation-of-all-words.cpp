09ea5a650a9e3f456f5974be9f807b7b3e10ecd0
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-09-22 12:24:13 +0530

    Time: 20 ms (70.24%) | Memory: 23.9 MB (68.06%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.cpp
@@ -7,57 +7,59 @@ public:
         int len = words[0].size();
         int n = words.size();
 
-        unordered_map<string, int> need;
+        unordered_map<string ,int> need;
 
-        // Har word kitni baar chahiye.
-        for (string word : words)
-            need[word]++;
+        //har word kitni baar aya
+        
+        for(string word : words)
+         need[word]++;
 
-        // Different starting alignments check karo.
-        for (int start = 0; start < len; start++) {
+         //different alignment
 
-            unordered_map<string, int> have;
+         for(int start = 0 ; start < len ; start++){
+            unordered_map<string , int> have;
+
             int left = start;
             int count = 0;
 
-            // Ek-ek complete word read karo.
-            for (int right = start; right + len <= s.size(); right += len) {
+            //one by one word read
 
-                string word = s.substr(right, len);
+            for(int right = start ; right + len <= s.size() ; right += len){
+                //required word or not
 
-                // Ye word required hi nahi hai.
-                if (!need.count(word)) {
+                string word = s.substr(right , len);
+
+                if(!need.count(word)){
                     have.clear();
                     count = 0;
                     left = right + len;
                     continue;
                 }
 
-                // Word window mein add karo.
                 have[word]++;
                 count++;
 
-                // Word required se zyada aa gaya.
-                while (have[word] > need[word]) {
-                    string old = s.substr(left, len);
+                //word window add 
+
+                while(have[word] > need[word]){
+                    string old  = s.substr(left , len);
                     have[old]--;
                     left += len;
                     count--;
                 }
 
-                // Saare words exactly mil gaye.
-                if (count == n) {
+                if(count == n){
                     ans.push_back(left);
 
-                    // Next possible window ke liye left word hatao.
-                    string old = s.substr(left, len);
+                    string old = s.substr(left , len);
                     have[old]--;
                     left += len;
                     count--;
                 }
             }
-        }
+         }
 
-        return ans;
+         return ans;
+        
     }
 };
