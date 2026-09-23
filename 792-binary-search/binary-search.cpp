1d4e2b657ba46aa5b7790260b089a4f85790346e
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-09-23 11:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (6.83%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
         int left = 0;
-        int right = (nums.size()) - 1;
-
-        while (left <= right) {
-
-            // Overflow-safe middle index.
-            int mid = left + (right - left) / 2;
-
-            // Target found.
-            if (nums[mid] == target) {
+        int right = nums.size() - 1;
+        while(left <= right){
+            int mid = (left + (right - left)/2);
+            if( nums[mid] == target){
                 return mid;
             }
-
-            // Target can only exist in the right half.
-            if (nums[mid] < target) {
+            else if(nums[mid] < target){
                 left = mid + 1;
             }
-
-            // Target can only exist in the left half.
-            else {
+            else{
                 right = mid - 1;
             }
         }
-
-        // Search space became empty.
         return -1;
     }
 };
