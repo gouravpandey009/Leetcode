a2846644be24671c6387c705c3f4597a9a106df2
GOURAV PANDEY <pandeygourav6592@gmail.com>
2026-09-24 11:39:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (17.11%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,46 +5,48 @@ public:
         int first = -1;
         int last = -1;
 
-        // Find first occurrence.
+        //first occurenence
         int left = 0;
         int right = nums.size() - 1;
 
-        while (left <= right) {
+        while(left <= right){
+            int mid = left + (right - left) /2;
 
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
+            if(nums[mid] == target){
                 first = mid;
-                right = mid - 1;   // Aur left mein check karo.
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else {
                 right = mid - 1;
+
+            } else if (nums[mid] < target) {
+                left =  mid + 1;
+
+            } else {
+                right = mid -1;
             }
         }
 
-        // Find last occurrence.
+        //last occurence
+
         left = 0;
-        right = nums.size() - 1;
+        right = nums.size() -1;
 
-        while (left <= right) {
+        while(left <= right){
+            int mid = left + (right - left) /2;
 
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
+            if(nums[mid] == target){
                 last = mid;
-                left = mid + 1;    // Aur right mein check karo.
-            }
-            else if (nums[mid] < target) {
                 left = mid + 1;
-            }
-            else {
-                right = mid - 1;
+
+            } else if (nums[mid] < target) {
+                left =  mid + 1;
+
+            } else {
+                right = mid -1;
             }
         }
 
-        return {first, last};
+
+        return {first , last};
+
+        
     }
 };
